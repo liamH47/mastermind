abda8c0d86d281344eaae2f6bf65f90e69a0b4ba
unknown <liamhanafee@gmail.com>
2021-05-12 20:48:49 -0400

    backend somewhat set up. successfully console logging the random number after api call

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,47 +1,4 @@
-// import React, { useState } from 'react';
-// import Select from './Select';
 
-// const Form = () => {
-//     const [formData, setFormData] = useState({
-//         firstNum: '',
-//         secondNum: '',
-//         thirdNum: '',
-//         fourthNum: ''
-//     })
-
-//     const submitHandler = (e) => {
-//         e.preventDefault();
-//         console.log(formData);
-//     }
-
-//     return (
-//         <form onSubmit={submitHandler}>
-//             <Select 
-//                 onChange={(e) => setFormData({...formData, firstNum: e.target.value })}
-//                 value={formData.firstNum}
-//                 name='firstNum'
-//             />
-//             <Select 
-//                 onChange={(e) => setFormData({...formData, secondNum: e.target.value })}
-//                 value={formData.secondNum}
-//                 name='secondNum'
-//             />
-//             <Select 
-//                 onChange={(e) => setFormData({...formData, thirdNum: e.target.value })}
-//                 value={formData.thirdNum}
-//                 name='thirdNum'
-//             />
-//             <Select 
-//                 onChange={(e) => setFormData({...formData, fourthNum: e.target.value })}
-//                 value={formData.fourthNum}
-//                 name='fourthNum'
-//             />
-//             <button type='submit'>Submit</button>
-//         </form>
-//     )
-// }
-
-// export default Form
 import React, { Component } from 'react'
 
 class Form extends Component {
@@ -86,7 +43,3 @@ class Form extends Component {
 }
 
 export default Form
-
-/**
- * need to figure out why the formdata is not working. maybe try using a class component instead
- */
---
+++ src/Components/GuessList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class GuessList extends Component {
+    render() {
+        return (
+            <ul>
+                
+            </ul>
+        )
+    }
+}
+
+export default GuessList;
